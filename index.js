778e57ad51db6e2823e9d3b98b14784925e229ce
awnishd <121890475+awnishd@users.noreply.github.com>
2023-02-01 08:58:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import Connection from './database/db.js'
+import cors from 'cors';
+import Routes from './routes/route.js'
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', Routes);
+const PORT = process.env.PORT||8000;
+Connection();
+
+app.listen(PORT, () => { console.log(`Server is running on PORT ${8000}`) });
